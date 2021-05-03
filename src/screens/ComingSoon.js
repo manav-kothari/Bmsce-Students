b73acbe268f8dcc444c91aaed3b5a3ca4d0f2af9
manav-kothari <manavkothari777@gmail.com>
2021-05-03 18:34:48 +0530

    updates coming soon screen

--- src/screens/ComingSoon.js
+++ src/screens/ComingSoon.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const ComingSoon = () => {
   return (
-    <div className="aler alert-info text-center blink_me p-3 my-5 text-capitalize h1 text-dark">
-      Notes Coming Soon...
-    </div>
+    <>
+      {" "}
+      <div className="aler alert-info text-center blink_me p-3 my-5 text-capitalize h1 text-dark">
+        Notes Coming Soon...
+      </div>
+      <div className="p-4 text-center">
+        If you are having any notes or links related to your course then kindly{" "}
+        <a href="https://www.instagram.com/bmscestudents/">
+          <button className="btn-danger">DM</button>
+        </a>{" "}
+        us or mail us at: <p className="text-info">bmscestudents@gmail.com</p>
+      </div>
+    </>
   );
 };
 
