e0ca260665d96c46ee7feffe21f2f02a61f6fe0f
manav-kothari <manavkothari777@gmail.com>
2021-05-08 20:11:14 +0530

    updated event

--- src/events.js
+++ src/events.js
@@ -1,149 +1,79 @@
 const events = [
-  {
-    _id: "1",
-    name: "A Business Model Canvas",
-    image: "/images/events/iic.jpg",
-    description: (
-      <p>
-        Greetings from BMSCE IIC, the start-up cell of B.M.S College of
-        Engineering!😃
-        <br />
-        Got a new business idea or an entrepreneurial dream, but don't know how
-        to put it to work? Here's the good news! BMSCE IIC in collaboration with
-        the IVY Club, brings to you a project-centred course, where participants
-        will use the Business Model Canvas innovation tool to approach a
-        business idea.💫
-        <br />
-        A business model canvas is a quick-start alternative to a business plan.
-        It helps you think through the most important aspects of startup—such as
-        your product or service, your target market, and the resources you'll
-        need to launch—in a brief chart format
-        <br />
-        🔹️By the end of the session participants will be able to identify and
-        communicate the nine key elements of a business model: ``Customer
-        Segments, Value Proposition, Channels, Customer Relationships, Key
-        Resources, Key Activities, Key Partners, Revenue Streams and Cost
-        Structure`` and develop their very own projects!!🌟
-        <br />
-        Course Instructor: Nida Sahar, Founder Nife, Alumni IIMB, BMSCE
-        <br />
-        Date: *7th May*,2021
-        <br />
-        Time🕕: *6 to 8 PM*
-        <br />
-        Open to all !!🎉
-        <br /> Join:
-        <a href="https://chat.whatsapp.com/IUmc6HCcHl6JcwZb9aP0ch">
-          <button className="btn-success">WhatsApp Group</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "2",
-    name: "INTERNSHIP PROGRAM",
-    image: "/images/events/ieee.jpg",
-    description: (
-      <p>
-        INTERNSHIP PROGRAM
-        <br />
-        Tequed Labs, a Nasscom incubated Edtech Company in collaboration with
-        BMSCE IEEE is bringing to you Industrial Skill Development and Summer
-        Internship Program 2021. What's it all about?
-        <br />
-        This is a 1 month internship program which includes a classroom course
-        for a week and 3 weeks of project.
-        <br />
-        The following domains are offered:
-        <br />
-        1. Internet of Things
-        <br />
-        2. Artificial Intelligence and Machine Learning
-        <br />
-        3. Full Stack Web Development
-        <br />
-        4. Cyber Security and Ethical Hacking
-        <br />
-        5. 3D Modelling
-        <br />
-        6. Mobile App Development
-        <br />
-        The program will commence soon after 2nd/4th/6th SEE Exams.
-        <br />
-        Each participant will receive a Certificate of Course completion and a
-        Certificate of Internship.
-        <br />
-        Best projects will also be awarded and top performers will receive Best
-        Intern Awards along with Job Opportunities.
-        <br />
-        Registration fee:
-        <br />
-        IEEE members : Rs. 3500/-
-        <br />
-        Non IEEE members : Rs. 4500/-
-        <br />
-        Group Discounts:
-        <br />
-        • 5 members: each member of the group gets Rs. 200/- off
-        <br />
-        • 10 members: each member of the group gets Rs. 500/- off
-        <br />
-        To avail group discounts, please contact, +918088219317
-        <br />
-        In addition to the afore mentioned benefits, IEEE members gain free
-        access to online courses worth Rs. 1500/-.
-        <br />
-        Book your seat by paying only Rs. 500/-. Remaining amount can be paid
-        later.
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfQxhl1iv8G3pEofNlCru5i66y3QbgHu0FhhKiKjA5HXPf0DA/viewform">
-          <button className="btn-info">Register now!</button>
-        </a>{" "}
-        For queries contact,
-        <br />
-        Nitin: 8088219317
-        <br />
-        Samartha: 9738473489
-        <br />
-        Detailed information, fill the{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "3",
-    name: "Learn to design a PCB from scratch",
-    image: "/images/events/inovact2.jpeg",
-    description: (
-      <p>
-        As Electronic devices are getting compact, PCB designing is gaining more
-        importance and popularity.
-        <br />
-        Learn to design a PCB from scratch as per Industry Standards from the
-        webinar offered by Inovact on PCB DESIGN.
-        <br />
-        Date:- 08/05/2021
-        <br />
-        Time:- 11:00AM- 1:30PM
-        <br />
-        Webinar fee:-₹49 ONLY
-        <br />
-        E-certificates will be provided for all participants.
-        <br />
-        Registration link to the webinar:- <br />
-        <a href="https://forms.gle/zenvqdzqW5EnL6Bs9">
-          <button className="btn-info">FORM</button>
-        </a>{" "}
-        <br />
-        For any queries,contact:
-        <br />
-        Rahul : 9742168308
-        <br />
-        Nabil : 974041798
-        <br />
-      </p>
-    ),
-  },
+  // {
+  //   _id: "1",
+  //   name: "A Business Model Canvas",
+  //   image: "/images/events/iic.jpg",
+  //   description: (
+  //     <p>
+  //       Greetings from BMSCE IIC, the start-up cell of B.M.S College of
+  //       Engineering!😃
+  //       <br />
+  //       Got a new business idea or an entrepreneurial dream, but don't know how
+  //       to put it to work? Here's the good news! BMSCE IIC in collaboration with
+  //       the IVY Club, brings to you a project-centred course, where participants
+  //       will use the Business Model Canvas innovation tool to approach a
+  //       business idea.💫
+  //       <br />
+  //       A business model canvas is a quick-start alternative to a business plan.
+  //       It helps you think through the most important aspects of startup—such as
+  //       your product or service, your target market, and the resources you'll
+  //       need to launch—in a brief chart format
+  //       <br />
+  //       🔹️By the end of the session participants will be able to identify and
+  //       communicate the nine key elements of a business model: ``Customer
+  //       Segments, Value Proposition, Channels, Customer Relationships, Key
+  //       Resources, Key Activities, Key Partners, Revenue Streams and Cost
+  //       Structure`` and develop their very own projects!!🌟
+  //       <br />
+  //       Course Instructor: Nida Sahar, Founder Nife, Alumni IIMB, BMSCE
+  //       <br />
+  //       Date: *7th May*,2021
+  //       <br />
+  //       Time🕕: *6 to 8 PM*
+  //       <br />
+  //       Open to all !!🎉
+  //       <br /> Join:
+  //       <a href="https://chat.whatsapp.com/IUmc6HCcHl6JcwZb9aP0ch">
+  //         <button className="btn-success">WhatsApp Group</button>
+  //       </a>{" "}
+  //     </p>
+  //   ),
+  // },
+
+  // {
+  //   _id: "3",
+  //   name: "Learn to design a PCB from scratch",
+  //   image: "/images/events/inovact2.jpeg",
+  //   description: (
+  //     <p>
+  //       As Electronic devices are getting compact, PCB designing is gaining more
+  //       importance and popularity.
+  //       <br />
+  //       Learn to design a PCB from scratch as per Industry Standards from the
+  //       webinar offered by Inovact on PCB DESIGN.
+  //       <br />
+  //       Date:- 08/05/2021
+  //       <br />
+  //       Time:- 11:00AM- 1:30PM
+  //       <br />
+  //       Webinar fee:-₹49 ONLY
+  //       <br />
+  //       E-certificates will be provided for all participants.
+  //       <br />
+  //       Registration link to the webinar:- <br />
+  //       <a href="https://forms.gle/zenvqdzqW5EnL6Bs9">
+  //         <button className="btn-info">FORM</button>
+  //       </a>{" "}
+  //       <br />
+  //       For any queries,contact:
+  //       <br />
+  //       Rahul : 9742168308
+  //       <br />
+  //       Nabil : 974041798
+  //       <br />
+  //     </p>
+  //   ),
+  // },
   {
     _id: "4",
     name: "Fundamentals of Digital Marketing",
@@ -260,6 +190,77 @@ const events = [
         </a>{" "}
         😉
         <br />
+      </p>
+    ),
+  },
+  {
+    _id: "10",
+    name: "INTERNSHIP PROGRAM",
+    image: "/images/events/ieee.jpg",
+    description: (
+      <p>
+        INTERNSHIP PROGRAM
+        <br />
+        Tequed Labs, a Nasscom incubated Edtech Company in collaboration with
+        BMSCE IEEE is bringing to you Industrial Skill Development and Summer
+        Internship Program 2021. What's it all about?
+        <br />
+        This is a 1 month internship program which includes a classroom course
+        for a week and 3 weeks of project.
+        <br />
+        The following domains are offered:
+        <br />
+        1. Internet of Things
+        <br />
+        2. Artificial Intelligence and Machine Learning
+        <br />
+        3. Full Stack Web Development
+        <br />
+        4. Cyber Security and Ethical Hacking
+        <br />
+        5. 3D Modelling
+        <br />
+        6. Mobile App Development
+        <br />
+        The program will commence soon after 2nd/4th/6th SEE Exams.
+        <br />
+        Each participant will receive a Certificate of Course completion and a
+        Certificate of Internship.
+        <br />
+        Best projects will also be awarded and top performers will receive Best
+        Intern Awards along with Job Opportunities.
+        <br />
+        Registration fee:
+        <br />
+        IEEE members : Rs. 3500/-
+        <br />
+        Non IEEE members : Rs. 4500/-
+        <br />
+        Group Discounts:
+        <br />
+        • 5 members: each member of the group gets Rs. 200/- off
+        <br />
+        • 10 members: each member of the group gets Rs. 500/- off
+        <br />
+        To avail group discounts, please contact, +918088219317
+        <br />
+        In addition to the afore mentioned benefits, IEEE members gain free
+        access to online courses worth Rs. 1500/-.
+        <br />
+        Book your seat by paying only Rs. 500/-. Remaining amount can be paid
+        later.
+        <br />
+        <br />
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfQxhl1iv8G3pEofNlCru5i66y3QbgHu0FhhKiKjA5HXPf0DA/viewform">
+          <button className="btn-info">Register now!</button>
+        </a>{" "}
+        For queries contact,
+        <br />
+        Nitin: 8088219317
+        <br />
+        Samartha: 9738473489
+        <br />
+        Detailed information, fill the{" "}
       </p>
     ),
   },
