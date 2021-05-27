595a4fb0a6bbecaf1f289c2b221b35c20da1564c
manav-kothari <manavkothari777@gmail.com>
2021-05-27 12:50:31 +0530

    updated event & notes screen

--- src/components/NoteCardComponent.js
+++ src/components/NoteCardComponent.js
@@ -5,7 +5,10 @@ const NoteCardComponent = ({ note }) => {
   return (
     <>
       <Card className="my-2 p-2 mx-2  rounded cards">
-        <a style={{ color: "#000" }} href={`/comingsoon`}>
+        <a
+          style={{ color: "#000" }}
+          href={`https://drive.google.com/drive/u/4/folders/1yJFWOSbWhnlpiu66cK8dBsfBe6X-L5Sg`}
+        >
           <Card.Img
             className="note-card-img-top embed-responsive-item"
             variant="top"
@@ -13,14 +16,20 @@ const NoteCardComponent = ({ note }) => {
           />
         </a>
         <Card.Body className="text-center p-2">
-          <a style={{ color: "#000" }} href={`/comingsoon`}>
+          <a
+            style={{ color: "#000" }}
+            href={`https://drive.google.com/drive/u/4/folders/1yJFWOSbWhnlpiu66cK8dBsfBe6X-L5Sg`}
+          >
             <Card.Title>
               <strong style={{ color: "#000" }} className="text-dark">
                 {note.name}
               </strong>
             </Card.Title>
           </a>
-          <a style={{ color: "#000" }} href={`/comingsoon`}>
+          <a
+            style={{ color: "#000" }}
+            href={`https://drive.google.com/drive/u/4/folders/1yJFWOSbWhnlpiu66cK8dBsfBe6X-L5Sg`}
+          >
             <Button
               style={{ backgroundColor: "#000" }}
               className="text-capitalize btn-block custom-btn"
--- src/events.js
+++ src/events.js
@@ -1,5 +1,34 @@
 const events = [
   {
+    _id: "0",
+    name: "Upcoming event by IEEE",
+    image: "/images/events/ieee.gif",
+    description: (
+      <p>
+        Greetings from BMSCE IEEE!
+        <br />
+        The lockdown has been a buzzkill. But, we have something to lighten your
+        mood and give your brain a little work. <br />
+        So, fire up your neurons! <br />
+        This is the trailer for an upcoming event. The catch is, the name of the
+        event is in the form of various ciphers. The task here is to decipher it
+        and mention at least 4 of the ciphers used.
+        <br />
+        The first 5 people to successfully complete this task will have an added
+        advantage over the others in the actual event.
+        <br />
+        So, can you crack it?
+        <br />
+        To participate in this task, fill the Google Form in the description of
+        the{" "}
+        <a className="btn-danger btn" href="https://youtu.be/PIdeKP4hgVo">
+          video!
+        </a>{" "}
+      </p>
+    ),
+  },
+
+  {
     _id: "1",
     name: "Mandala Art Workshop",
     image: "/images/events/rota.jpg",
@@ -38,17 +67,35 @@ const events = [
     image: "/images/events/fashion.jpg",
     description: (
       <p>
-        The google form will be open from 31/5/21 to 3/6/21 only.
+        Send a 15-20 sec video for each of the songs mentioned below with a
+        maximum of 2 poses (reference videos will be sent soon !)
         <br />
-        Check out{" "}
-        <a
-          className="btn-danger btn my-2"
-          href="https://www.instagram.com/p/CPKnD73jud1/?utm_source=ig_web_copy_link"
-        >
-          Rules
+        Girls: Say so by Doja Cat and <br />
+        Trampoline by Zayn Malik Boys: Adventure of a lifetime - Coldplay and{" "}
+        <br />
+        Starboy - The Weeknd Please attach both the videos as one single google
+        photos link on google form
+        <br />
+        •Send a full body image in portrait mode on google form
+        <br />
+        •Fill in all the details provided in the google form.
+        <br /> •No sort of editing or filters will be entertained in the video/
+        photo.
+        <br />
+        •Make sure you walk for the musics that is provided to you by us <br />
+        •Makeup/ hairstyle artists must attach 3 works on theirs as one google
+        photo link on google form.
+        <br />
+        The google link will be open from 31/5/21 to 3/6/21 only.
+        <br />
+        Reference videos will be sent soon.
+        <br />
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSef846LDPkV5UewzuQbaavNjG2mfCiW1Bqh_L5B3QrUYhr0jQ/viewform">
+          <button className="btn-info">FORM</button>
         </a>{" "}
         <br />
-        For more information check out instagram page:
+        Follow the BMSCE fashion team page on Instagram for more updates and
+        check our highlights if there are any concerns:
         <a
           className="text-info"
           href="https://www.instagram.com/bmsce.fashionteam/"
@@ -115,6 +162,181 @@ const events = [
         Rakshitha S: 8884767901
         <br />
         Mohit Raj : 81471 97203
+      </p>
+    ),
+  },
+
+  {
+    _id: "4",
+    name: "Webinar on Entrepreneurship Development",
+    image: "/images/events/invoe.jpeg",
+    description: (
+      <p>
+        Hello! Build leadership skills and develop an entrepreneurial mindset
+        with Inovact . <br />
+        We are conducting a webinar on Entrepreneurship Development which will
+        provide first hand experience on 'What it takes to be a true
+        entrepreneur'. <br />
+        Instructor: Mr.Raiba Bhosale , Co-Founder at Inovact. <br />
+        Webinar Deliverables: <br />
+        -Introduction to Entrepreneurship <br />
+        -Team formation <br />
+        -Effectuation <br />
+        -Ideation methods <br />
+        -Idea Prototyping <br />
+        -Opportunity identification <br />
+        -Customer segmentation <br />
+        -Value proposition <br />
+        -Marketing 101 <br />
+        -Finance 101 <br />
+        -Business model canvas <br />
+        -Pitching ideas <br />
+        <br />
+        Date:- 29/05/2021
+        <br />
+        Time:- 11:00AM- 1:30PM
+        <br />
+        Webinar fee:- ₹99 ONLY
+        <br />
+        Inovact will be offering Post-Webinar Activities for 1 week with
+        mentorship, absolutely for free!
+        <br />
+        *E-Certificates will be provided for ALL participants*
+        <br />
+        <a href="https://forms.gle/HChbxhVz6rwruf5r6">
+          <button className="btn-info">Register</button>
+        </a>{" "}
+        <br />
+        For more information contact: Rahul : 9742168308
+        <br />
+        Follow us on
+        <a
+          className="text-danger"
+          href="https://instagram.com/inovact__?utm_medium=copy_link"
+        >
+          {" "}
+          Instagram
+        </a>{" "}
+      </p>
+    ),
+  },
+
+  {
+    _id: "5",
+    name: "Webinar on 'Machine Learning'",
+    image: "/images/events/invoml.jpeg",
+    description: (
+      <p>
+        Hello! Learn from scratch and strengthen your fundamentals on Machine
+        Learning with experienced Instructors and Kick-off your career in
+        Machine learning.
+        <br />
+        INOVACT is offering a 2 days Webinar on 'Machine Learning' as a part of
+        'Build a skill for the future' series.
+        <br />
+        Instructor: Mr.Prathik Bafna , Tech enthusiast and ML Instructor.
+        <br />
+        Webinar Deliverables:
+        <br />
+        •Data Pre-processing
+        <br />
+        •Supervised Machine Learning Algorithms
+        <br />
+        • Regression
+        <br />
+        • Decision Trees
+        <br />
+        • Random Forest
+        <br />
+        • Evaluating the regression models
+        <br />
+        • Logistic Regression
+        <br />
+        • K-Nearest Neighbors
+        <br />
+        • Support Vector Machine (SVM)
+        <br />
+        • Kernel SVM
+        <br />
+        Date: 29/05/21
+        <br />
+        Time: 11am - 1pm <br />
+        WEBINAR FEE: 599 ₹99 ONLY <br />
+        Pre-Requisites : Basic Python programming <br />
+        Inovact will be offering Post-Webinar Assessment and Activities for 1
+        week with mentorship, absolutely for free!
+        <br />
+        *E-Certificates will be provided for ALL participants*
+        <br />
+        Kindly fill the form to confirm your seat:
+        <br />
+        <a href="https://forms.gle/jUPKv9wuVgkEiE6T9">
+          <button className="btn-info">FORM</button>
+        </a>{" "}
+        <br />
+        For more information contact: Rahul : 9742168308
+        <br />
+        Follow us on
+        <a
+          className="text-danger"
+          href="https://instagram.com/inovact__?utm_medium=copy_link"
+        >
+          {" "}
+          Instagram
+        </a>{" "}
+      </p>
+    ),
+  },
+
+  {
+    _id: "6",
+    name: "A Transformational Journey of Breakthrough Ideas to Successful Products",
+    image: "/images/events/ciie.jpg",
+    description: (
+      <p>
+        Hey Whiz Kid 🧠
+        <br />
+        Are you in a dilemma about how to take your unique and mind blowing 🤯
+        ideas 💡 forward? Do you often wonder about the road less traveled? Are
+        you at a roadblock 🚧 with respect to your wonderful idea since you
+        don’t know what to do about it, and how to take it forward ⏩?
+        <br />
+        Well, it’s party time 🥳 ‘cuz we know exactly how to steer 🚘 you to
+        your path of discovery ✨.
+        <br />
+        Here’s presenting BMSCE CIIE’s first ever hands-on workshop spanned over
+        an engaging 5 days on “A Transformational Journey of Breakthrough Ideas
+        to Successful Products”, conducted by Prof. Madhav Murthy from the Dept.
+        of Mechanical Engg in association with BMSCE’s Center for Innovation,
+        Incubation and Entrepreneurship (CIIE).
+        <br />
+        Sign up 🔜 as there are only limited slots! Join us as we help you turn
+        your vision into value ✨
+        <br />
+        Registration link:
+        <a className="text-info" href="https://ciie.bmsedu.in/">
+          {" "}
+          https://ciie.bmsedu.in/
+        </a>{" "}
+        Participation certificate provided for 100% attendance and assignment
+        completion Follow us on: <br />
+        Instagram-{" "}
+        <a className="text-danger" href="https://instagram.com/ciie_bmsce">
+          {" "}
+          @ciie_bmsce
+        </a>{" "}
+        <br />
+        Linkedln- @BMSCE Center for Innovation Incubation and Entrepreneurship
+        (CIIE)
+        <br />
+        Any queries? <br />
+        Contact:
+        <br />
+        Spoorti- 8296083493
+        <br />
+        Abhyuday- 8310986565
+        <br />
+        Ritu- 9741466600
       </p>
     ),
   },
