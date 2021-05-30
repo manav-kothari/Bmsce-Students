122d12bf38d5bb2d29ac99160108d526d83d05b1
manav-kothari <manavkothari777@gmail.com>
2021-05-30 12:39:31 +0530

    updated event

--- src/events.js
+++ src/events.js
@@ -1,62 +1,50 @@
 const events = [
   {
-    _id: "0",
+    _id: "1",
     name: "Upcoming event by IEEE",
     image: "/images/events/ieee.gif",
     description: (
       <p>
-        Greetings from BMSCE IEEE!
+        "The ability to learn faster than your competitors may be only
+        sustainable competitive advantage." -Arie de Geus
         <br />
-        The lockdown has been a buzzkill. But, we have something to lighten your
-        mood and give your brain a little work. <br />
-        So, fire up your neurons! <br />
-        This is the trailer for an upcoming event. The catch is, the name of the
-        event is in the form of various ciphers. The task here is to decipher it
-        and mention at least 4 of the ciphers used.
+        BMSCE IEEE presents to you "SCYTALE", a 24 hour cryptic adventure,
+        challenging your knowledge right from coding domains to everything new
+        and pioneering today, sponsored by CIIRC, an organisation promising to
+        usher a new philosophy of science and entrepreneurship.
         <br />
-        The first 5 people to successfully complete this task will have an added
-        advantage over the others in the actual event.
+        Participate in teams of two or individually.
         <br />
-        So, can you crack it?
+        Registration Fee:
         <br />
-        To participate in this task, fill the Google Form in the description of
-        the{" "}
-        <a className="btn-danger btn" href="https://youtu.be/PIdeKP4hgVo">
-          video!
+        Free for IEEE Members
+        <br />
+        ₹50 for each non-IEEE Member
+        <br />
+        Payments to be made at surabhihn27@okicici / 7899801301
+        <br />
+        The race starts at 6PM on 5th June, 2021.
+        <br />
+        Crack this cryptic hunt to win Amazon vouchers worth:
+        <br />
+        Winners: ₹2000
+        <br />
+        Runner up: ₹1500
+        <br />
+        Second runner up: ₹1000
+        <br />{" "}
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSc3ZeyAWIE3Tc1lMrVsQeAK1x3JzM2xmY_cwn2TpFXl57DEZA/viewform">
+          <button className="btn-info">Register</button>
         </a>{" "}
-      </p>
-    ),
-  },
-
-  {
-    _id: "1",
-    name: "Mandala Art Workshop",
-    image: "/images/events/rota.jpg",
-    description: (
-      <p>
-        Hola people ❤️
+        now for a thrilling experience!
         <br />
-        We release our next weekend activity "Talent Show"
+        Fill the form individually even if you are participating as a team.
         <br />
-        Do you have any unique talents like dance, singing, drama or any other
-        talent
+        For further queries, contact:
         <br />
-        Then this event is for you....Join us on 29-30 may at 7pm
+        Surabhi: 7899801301
         <br />
-        Kindly register yourself in the link provided for smooth systematic
-        process
-        <br />
-        Kindly register yourself in through <br />{" "}
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYAkn-Hlf0bBbTfaueJ-Wn6VdqOkNyMUqPrkiGzEwIvzIBiw/viewform">
-          <button className="btn-info">LINK</button>
-        </a>{" "}
-        <br />
-        <br />
-        Instagram:
-        <a className="text-info" href="https://www.instagram.com/rota_bms/">
-          {" "}
-          @rota_bms
-        </a>{" "}
+        Samartha: 9738473489
       </p>
     ),
   },
@@ -109,187 +97,6 @@ const events = [
 
   {
     _id: "3",
-    name: "Git Workshop",
-    image: "/images/events/ieee.png",
-    description: (
-      <p>
-        Greetings BMSCE!
-        <br />
-        BMSCE IEEE SB and BMSCE IEEE WIE present to you the Git Workshop.
-        <br />
-        What is it about?
-        <br />
-        Git is a software for tracking changes in set of files , usually
-        collaboration of programmers from different network to develop source
-        code during software development.
-        <br />
-        This workshop will also give you an idea as to how it is being used in
-        the IT industry and also how a code editor like vs code can use git and
-        make your programming life easy.
-        <br />
-        Prerequisites:
-        <br />
-        1.Git installed on your machine <br />
-        2.VS code <br />
-        3.Github profile <br />
-        <br />
-        Date: 29th May, 2021
-        <br />
-        Time: 3PM to 5PM
-        <br />
-        E-Certificates will be Provided
-        <br />
-        <br />
-        Payment details:
-        <br />
-        IEEE members: Free
-        <br />
-        Non-IEEE members: Rs.50/-
-        <br />
-        <br />
-        Payments to be made at rakshithamattuga@ybl / 8884767901.
-        <br />
-        After the payment, please do send the screenshot to 8884767901.
-        <br />
-        Do not let go of this wonderful opportunity! <br />
-        Limited seats only. Hurry up and{" "}
-        <a href="https://forms.gle/e9UqtSp614pRD48p8">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-        <br />
-        For queries, contact
-        <br />
-        Rakshitha S: 8884767901
-        <br />
-        Mohit Raj : 81471 97203
-      </p>
-    ),
-  },
-
-  {
-    _id: "4",
-    name: "Webinar on Entrepreneurship Development",
-    image: "/images/events/invoe.jpeg",
-    description: (
-      <p>
-        Hello! Build leadership skills and develop an entrepreneurial mindset
-        with Inovact . <br />
-        We are conducting a webinar on Entrepreneurship Development which will
-        provide first hand experience on 'What it takes to be a true
-        entrepreneur'. <br />
-        Instructor: Mr.Raiba Bhosale , Co-Founder at Inovact. <br />
-        Webinar Deliverables: <br />
-        -Introduction to Entrepreneurship <br />
-        -Team formation <br />
-        -Effectuation <br />
-        -Ideation methods <br />
-        -Idea Prototyping <br />
-        -Opportunity identification <br />
-        -Customer segmentation <br />
-        -Value proposition <br />
-        -Marketing 101 <br />
-        -Finance 101 <br />
-        -Business model canvas <br />
-        -Pitching ideas <br />
-        <br />
-        Date:- 29/05/2021
-        <br />
-        Time:- 11:00AM- 1:30PM
-        <br />
-        Webinar fee:- ₹99 ONLY
-        <br />
-        Inovact will be offering Post-Webinar Activities for 1 week with
-        mentorship, absolutely for free!
-        <br />
-        *E-Certificates will be provided for ALL participants*
-        <br />
-        <a href="https://forms.gle/HChbxhVz6rwruf5r6">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-        <br />
-        For more information contact: Rahul : 9742168308
-        <br />
-        Follow us on
-        <a
-          className="text-danger"
-          href="https://instagram.com/inovact__?utm_medium=copy_link"
-        >
-          {" "}
-          Instagram
-        </a>{" "}
-      </p>
-    ),
-  },
-
-  {
-    _id: "5",
-    name: "Webinar on 'Machine Learning'",
-    image: "/images/events/invoml.jpeg",
-    description: (
-      <p>
-        Hello! Learn from scratch and strengthen your fundamentals on Machine
-        Learning with experienced Instructors and Kick-off your career in
-        Machine learning.
-        <br />
-        INOVACT is offering a 2 days Webinar on 'Machine Learning' as a part of
-        'Build a skill for the future' series.
-        <br />
-        Instructor: Mr.Prathik Bafna , Tech enthusiast and ML Instructor.
-        <br />
-        Webinar Deliverables:
-        <br />
-        •Data Pre-processing
-        <br />
-        •Supervised Machine Learning Algorithms
-        <br />
-        • Regression
-        <br />
-        • Decision Trees
-        <br />
-        • Random Forest
-        <br />
-        • Evaluating the regression models
-        <br />
-        • Logistic Regression
-        <br />
-        • K-Nearest Neighbors
-        <br />
-        • Support Vector Machine (SVM)
-        <br />
-        • Kernel SVM
-        <br />
-        Date: 29/05/21
-        <br />
-        Time: 11am - 1pm <br />
-        WEBINAR FEE: 599 ₹99 ONLY <br />
-        Pre-Requisites : Basic Python programming <br />
-        Inovact will be offering Post-Webinar Assessment and Activities for 1
-        week with mentorship, absolutely for free!
-        <br />
-        *E-Certificates will be provided for ALL participants*
-        <br />
-        Kindly fill the form to confirm your seat:
-        <br />
-        <a href="https://forms.gle/jUPKv9wuVgkEiE6T9">
-          <button className="btn-info">FORM</button>
-        </a>{" "}
-        <br />
-        For more information contact: Rahul : 9742168308
-        <br />
-        Follow us on
-        <a
-          className="text-danger"
-          href="https://instagram.com/inovact__?utm_medium=copy_link"
-        >
-          {" "}
-          Instagram
-        </a>{" "}
-      </p>
-    ),
-  },
-
-  {
-    _id: "6",
     name: "Journey of Entrepreneurship",
     image: "/images/events/ciie30.jpg",
     description: (
@@ -322,71 +129,7 @@ const events = [
   },
 
   {
-    _id: "7",
-    name: "Webinar on 'INSIGHT INTO THE STOCK MARKET'",
-    image: "/images/events/invost.jpg",
-    description: (
-      <p>
-        Hello! Want to become financially independent but don't know what to
-        do!?
-        <br />
-        Don't worry!
-        <br />
-        Inovact is organizing a webinar on 'INSIGHT INTO THE STOCK MARKET' which
-        will help you climb the ladder of financial independence.
-        <br />
-        This webinar will guide you step by step towards entering the stock
-        market.
-        <br />
-        Instructor: Mr. Sarang , Co-Founder at Inovact with 3+ years of
-        experience as a stock market investor.
-        <br />
-        Webinar deliverables :<br />
-        -What is an investment? <br />
-        -Types of Investments
-        <br />
-        -What is the stock market ?<br />
-        -Mutual Funds and Company shares
-        <br />
-        -How to choose good companies for investing?
-        <br />
-        -What is stock Trading?
-        <br />
-        -Types of Trading.
-        <br />
-        -What is Fundamental Analysis and Technical Analysis?
-        <br />
-        -Reading charts
-        <br />
-        -Brokerage
-        <br />
-        -Market psychology
-        <br />
-        Date:- 29/05/2021 <br />
-        Time:- 11:00AM- 1:00PM <br />
-        Webinar fee:- ₹99 ONLY <br />
-        <a href="https://forms.gle/RDu2ugdYmXn9w2zp7">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-        <br />
-        Contact: Rahul : 9742168308
-        <br /> Follow us on Instagram:
-        <a
-          className="text-danger"
-          href="https://instagram.com/inovact__?utm_medium=copy_link"
-        >
-          {" "}
-          @inovact__
-        </a>{" "}
-        <br />
-        We are not a SEBI registered advisory. This is for educational purposes
-        only.
-      </p>
-    ),
-  },
-
-  {
-    _id: "8",
+    _id: "4",
     name: "#BeBoldForAChange 1.0",
     image: "/images/events/ciie03.jpg",
     description: (
@@ -428,7 +171,61 @@ const events = [
       </p>
     ),
   },
-
+  {
+    _id: "5",
+    name: "#BeBoldForAChange 1.0",
+    image: "/images/events/elsoc.jpeg",
+    description: (
+      <p>
+        Hello Everyone 💫
+        <br />
+        Interested in learning how to program and work with an Arduino, but
+        don’t know where to start? <br />
+        No better way to get to know a person other than dating them. 😉😉
+        <br />
+        ELSOC is back presenting to you ' A Date With' series with its first
+        episode 'A Date With Arduino'. 🥳🤩🥳
+        <br />
+        A two day workshop and bootcamp on Arduinos. In this workshop, we will
+        cover:
+        <br />
+        ✔Basics of Arduino
+        <br />
+        ✔Arduino simulators
+        <br />
+        ✔Basic sensors and their interfacing
+        <br />
+        ✔Basic actuators and their interfacing
+        <br />
+        Additionally, you will get hands-on experience by connecting and coding
+        your own circuits for defined problem statements. 💯🤩
+        <br />
+        Participantion Certificates for All 🤩🎊
+        <br />
+        Winners of the bootcamp will be given special certificates and shout-out
+        on our instagram page. 🎉🎉
+        <br />
+        Workshop: 3-6 pm, 5th June, 2021.
+        <br />
+        Bootcamp: 7 pm, 5th June, 2021 - 7 pm, 6th June, 2021. <br />
+        Requirements: Laptop with stable internet connection. 💻
+        <br />
+        Hurry up and fill the{" "}
+        <a href="https://forms.gle/nYNHKqxrQkea39XF7">
+          <button className="btn-info">FORM</button>
+        </a>{" "}
+        to register! ⏳⏳
+        <br />
+        Look for ELSOC discord server, after submitting the form 💫
+        <br />
+        For more details, contact:
+        <br />
+        Akhil: 9113991851
+        <br />
+        Bharath: 7406307010
+      </p>
+    ),
+  },
   {
     _id: "9",
     name: "Yoga for Unity & Well-Being",
@@ -533,5 +330,25 @@ const events = [
     ),
   },
 ];
+
+//***** INOVACT *****/
+// {
+//   _id: "5",
+//   name: "Webinar on 'Machine Learning'",
+//   image: "/images/events/invoml.jpeg",
+//   description: (
+//     <p>
+//
+//       Follow us on
+//       <a
+//         className="text-danger"
+//         href="https://instagram.com/inovact__?utm_medium=copy_link"
+//       >
+//         {" "}
+//         Instagram
+//       </a>{" "}
+//     </p>
+//   ),
+// },
 
 export default events;
