14bbf8da596b418e278a9218c58bafd0b9e4df1a
manav-kothari <manavkothari777@gmail.com>
2021-06-15 10:10:06 +0530

    updated event

--- src/events.js
+++ src/events.js
@@ -1,204 +1,6 @@
 const events = [
   {
-    _id: "8",
-    name: "Tutorial on Proteus Design Suite",
-    image: "/images/events/ieee2.jpg",
-    description:
-      "Get a chance to enhance your resume by gaining more knowledge about the the most important structural component of any automobile!!",
-    message: (
-      <p>
-        “It’s not that we use technology, we live technology” – Godfrey Reggio
-        Control processes to electromechanical integration, the evolution of
-        automation has made life simpler. BMSCE IEEE, in association with
-        LabCenter, is bringing to you a webinar on Tutorial on Proteus Design
-        Suite. A golden opportunity to master the skill of automation! What is
-        Proteus Design Suite? It is a proprietary software used primarily for
-        electronic design automation and to create schematics and electronic
-        prints for manufacturing PCBs. An essential skill that will be the
-        highlight of your skill set! All the attendees will gain *free licenses
-        of the software!*
-        <br />
-        Date : 11th June, 2021 Time : 2 PM to 4 PM Platform: Cisco WebEx It's
-        free for all! So, what are you waiting for? Grab this exciting
-        opportunity and <br />
-        <br />{" "}
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe8vi1oUZm2OglJZHkos3wP3xT65fV5tPvaJbLGhv_jGyP9w/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-        <br />
-        <br />
-        Check out{" "}
-        <a className="text-info" href="https://www.labcenter.com">
-          www.labcenter.com
-        </a>
-        , for more information about the software.
-        <br />
-        For further queries, contact: <br />
-        Samartha: 97384 73489 <br />
-        Mohit: 81471 97203 <br />
-        <br />
-      </p>
-    ),
-  },
-
-  {
     _id: "1",
-    name: "𝑯𝒂𝒄𝒌 𝑶’𝑯𝒐𝒍𝒊𝒄𝒔",
-    image: "/images/events/p1.jpg",
-    description:
-      "In this event, participants solve problems and code as per given specifications.",
-    message: (
-      <p>
-        | Protocol Day Events [1/6] | Here’s for you the first event, 𝑯𝒂𝒄𝒌
-        𝑶’𝑯𝒐𝒍𝒊𝒄𝒔, where participants solve problems and code as per given
-        specifications. Grab this opportunity to showcase your hacking skills!
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "6",
-    name: "Understanding Block chain, Cryptocurrency and Smart contracts",
-    image: "/images/events/ieee.jpg",
-    description:
-      "You'll be taught how to set up a wallet, do transactions and interact with a smart contract.",
-    message: (
-      <p>
-        BMSCE IEEE SB and BMSCE IEEE WIE bring to you a workshop Understanding
-        Block chain, Cryptocurrency and Smart contracts. What is it about?
-        Blockchain is buzzing technology which enables the existence of
-        cryptocurrencies which act as a medium of exchange. In this workshop,
-        you'll be taught how to set up a wallet, do transactions and interact
-        with a smart contract.
-        <br />
-        Date: 12th June, 2021
-        <br />
-        Time: 3PM to 7PM
-        <br />
-        Registration Fee:
-        <br />
-        IEEE students : Free
-        <br />
-        Non-IEEE students: Rs.50/-
-        <br />
-        Payments to be made at rakshithamattuga@ybl / 8884767901. Send a
-        screenshot of your payment to 8884767901.
-        <br />
-        Do not let go of this wonderful opportunity! Limited seats only. Hurry
-        up and{" "}
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfwdayB_JZ1s_C4U1dxZXyXOmy5IPGOffRI4w-syBo0Si4q6w/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-        <br />
-        For queries, contact:
-        <br />
-        Rakshitha S: 8884767901 <br />
-      </p>
-    ),
-  },
-  {
-    _id: "2",
-    name: "CRYPTO TALK",
-    image: "/images/events/p2.jpg",
-    description:
-      "The speaker for this workshop is Mr. Vineet Miskin who’s going to teach you about the basics of cryptocurrency",
-    message: (
-      <p>
-        The Ultimate Trading Guide.” The speaker for this workshop is Mr. Vineet
-        Miskin who’s going to teach you about the basics of cryptocurrency, the
-        understanding of the stock market, and trading in general.
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "0",
-    name: "No Typo",
-    image: "/images/events/no.jpg",
-    description: "All you have to do is to type! AS FAST AS YOU CAN!",
-    message: (
-      <p>
-        All you have to do is to type! Sounds interesting? But.. AS FAST AS YOU
-        CAN!
-        <br />
-        OPEN FOR ALL COLLEGES.
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "3",
-    name: "𝐁𝐨𝐭.𝐩𝐲🤖",
-    image: "/images/events/p3.jpg",
-    description:
-      "This event gives you the opportunity to make your own discord bot.",
-    message: (
-      <p>
-        Have you ever wondered how the groovy bot is able to play music or how
-        the MEE6 bot is able to automatically create custom commands? This event
-        “𝐁𝐨𝐭.𝐩𝐲🤖” gives you the opportunity to make your own discord bot.
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "4",
-    name: "𝑬𝒔𝒐𝑺𝒍𝒂𝒏𝒈",
-    image: "/images/events/p4.jpg",
-    description:
-      "In this event, you will be given the code in an alien language (esoteric) which you would have to decode into “C language” format.",
-    message: (
-      <p>
-        If not, a fun event named “𝑬𝒔𝒐𝑺𝒍𝒂𝒏𝒈” awaits for you! In this event, you
-        will be given the code in an alien language (esoteric) which you would
-        have to decode into “C language” format. You’re challenged to test the
-        boundaries of computer programming languages!
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-  {
-    _id: "5",
-    name: "Valorant Gaming Event",
-    image: "/images/events/p5.gif",
-    description:
-      "Keep your knives sharpened and your ammo intact because this is the “Valorant Gaming Event”! 🎮",
-    message: (
-      <p>
-        Bored in the house? There’s something exciting coming up for you! Keep
-        your knives sharpened and your ammo intact because this is the “Valorant
-        Gaming Event”! 🎮 Enjoy few stress free hours! Happy Gaming :)
-        <br />
-        <br />
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfqUOIsvrD48m34fq_dj0ilyLFm3ORpOYEmKlsLGXV8KQOu2A/viewform">
-          <button className="btn-info">Register</button>
-        </a>{" "}
-      </p>
-    ),
-  },
-
-  {
-    _id: "7",
     name: "CHASSIS- Design & Analysis",
     image: "/images/events/bull.jpg",
     description:
@@ -240,6 +42,37 @@ const events = [
         <br />
         Yuvaraj : 8123576036
         <br />
+      </p>
+    ),
+  },
+  {
+    _id: "2",
+    name: "Accelerate-2021",
+    image: "/images/events/iic.jpg",
+    description:
+      "Get a chance to enhance your resume by gaining more knowledge about the the most important structural component of any automobile!!",
+    message: (
+      <p>
+        Greetings from BMSCE IIC !🎉 ”The only source of knowledge is
+        experience." 💫Internships offer valuable real-world work experience and
+        new networking opportunities, but most importantly it offers a rare
+        opportunity for one to develop, learn, refine and apply their skill
+        sets.The importance and value of internships cannot be overstated, but
+        one must be prepared to make the most out of each opportunity. Here’s
+        the good news! BMSCE IIC in collaboration with the International Society
+        of Automation of BMSCE has organised an interactive panel discussion
+        with 5 industry and education experts to help you. Maximize the benefits
+        and learning from Internships. Date: 19.06.20 Timings: 5 - 7 PM Open to
+        all! Do not miss out on this wonderful opportunity! ✨
+        <br />
+        <br />{" "}
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdYSjZOhyBNMpy0ZyDg_xsWXdWsgQ2XK9scHsX_Hi2chEP2-A/viewform">
+          <button className="btn-info">Register</button>
+        </a>{" "}
+        now
+        <br />
+        <br />
+        Contact: Dheeraj: 6362468427 <br />
       </p>
     ),
   },
